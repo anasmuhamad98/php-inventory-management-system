b2e62f80c9d2a1b8da911df9abef1b1b30b44123
Amirah Fathiah <amirahfathiah024@gmail.com>
2021-01-03 06:11:42 +0800

    product ranking

--- php_action/db_connect.php
+++ php_action/db_connect.php
@@ -3,7 +3,7 @@
 $localhost = "localhost";
 $username = "root";
 $password = "";
-$dbname = "test";
+$dbname = "store";
 $store_url = "http://localhost/php-inventory-management-system/";
 // db connection
 $connect = new mysqli($localhost, $username, $password, $dbname);
