c29852ce12c4cd71337c61951b357ef9f7d5254b
AHMAD ANAS EZZAT BIN MUHAMAD <anasmuhamad98@gmail.com>
2021-01-02 10:21:35 +0800

    test for pull

--- promotion.php
+++ promotion.php
@@ -10,7 +10,30 @@
 		</ol>
 	</div>
 </div>
-<!-- test sajo2 untuk nok main pull and merge request -->
+
+<!-- categories brand -->
+<div class="modal fade" tabindex="-1" role="dialog" id="removeProductModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title"><i class="glyphicon glyphicon-trash"></i> Remove Product</h4>
+      </div>
+      <div class="modal-body">
+
+      	<div class="removeProductMessages"></div>
+
+        <p>Do you really want to remove ?</p>
+      </div>
+      <div class="modal-footer removeProductFooter">
+        <button type="button" class="btn btn-default" data-dismiss="modal"> <i class="glyphicon glyphicon-remove-sign"></i> Close</button>
+        <button type="button" class="btn btn-primary" id="removeProductBtn" data-loading-text="Loading..."> <i class="glyphicon glyphicon-ok-sign"></i> Save changes</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- /categories brand -->
+
 <script src="custom/js/promotion.js"></script>
 
 <?php require_once 'includes/footer.php'; ?>
