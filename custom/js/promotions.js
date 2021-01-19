df23f67980236dfe4d1df17134addd467d3a3dfb
AHMAD ANAS EZZAT BIN MUHAMAD <anasmuhamad98@gmail.com>
2021-01-19 19:28:46 +0800

    evo part

---
+++ custom/js/promotions.js
@@ -0,0 +1,13 @@
+var managePromotionTable;
+
+$(document).ready(function() {
+	// top nav bar 
+	$('#navPromo').addClass('active');
+
+	// manage promotion table
+	managePromotionTable = $("#managePromotionTable").DataTable({
+		'ajax': 'php_action/fetchPromotion.php',
+		'order': []		
+	});
+
+}); // document.ready fucntion
--- php_action/fetchpromotion.php
+++ php_action/fetchpromotion.php
@@ -1,84 +1,75 @@
-<?php 	
-
-
+<?php
 
 require_once 'core.php';
 
-$sql = "SELECT product.product_id, product.product_name, product.product_image, product.brand_id,
- 		product.categories_id, product.quantity, product.rate, product.active, product.status, 
- 		brands.brand_name, categories.categories_name FROM product 
-		INNER JOIN brands ON product.brand_id = brands.brand_id 
-		INNER JOIN categories ON product.categories_id = categories.categories_id  
-		WHERE product.status = 1 AND product.quantity>0";
-
+$sql = "SELECT * FROM promotions WHERE promo_status = 1";
 $result = $connect->query($sql);
-
 $output = array('data' => array());
 
-if($result->num_rows > 0) { 
+$promoItemsql = "SELECT * FROM promotion_item WHERE promo_item_status = 1";
+$promoresult = $connect->query($promoItemsql);
+$promoItemoutput = array();
 
- // $row = $result->fetch_array();
- $active = ""; 
 
- while($row = $result->fetch_array()) {
- 	$productId = $row[0];
- 	// active 
- 	if($row[7] == 1) {
- 		// activate member
- 		$active = "<label class='label label-success'>Available</label>";
- 	} else {
- 		// deactivate member
- 		$active = "<label class='label label-danger'>Not Available</label>";
- 	} // /else
 
- 	$button = '<!-- Single button -->
+
+if ($result->num_rows > 0) {
+
+	// $row = $result->fetch_array();
+	$statusPromo = "";
+	$x = 1;
+
+	while ($row = $result->fetch_array()) {
+		$promoId = $row[0];
+		//to get promo_items
+		$promoItem = "";
+
+		if ($promoresult->num_rows > 0) {
+		while ($row2 = $promoresult->fetch_array()) {
+			$promoItemId = "";
+			if ($row2[1] == $promoId) {
+				$productsql = "SELECT * FROM product WHERE product_id = $row2[2]";
+				$productresult = $connect->query($productsql);
+				$row3 = $productresult->fetch_array();
+				$items = $row3[1];
+				$promoItemoutput[] = array(
+					" {$items}",
+				);
+					}
+			}
+		}
+
+		// active 
+		if ($row[2] == 1) {
+			// activate member
+			$statusPromo = "<label class='label label-success'>Available</label>";
+		} else {
+			// deactivate member
+			$statusPromo = "<label class='label label-danger'>Not Available</label>";
+		}
+
+		$button = '<!-- Single button -->
 	<div class="btn-group">
 	  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 	    Action <span class="caret"></span>
 	  </button>
 	  <ul class="dropdown-menu">
-	    <li><a type="button" data-toggle="modal" id="editProductModalBtn" data-target="#editProductModal" onclick="editProduct('.$productId.')"> <i class="glyphicon glyphicon-edit"></i> Edit</a></li>
-	    <li><a type="button" data-toggle="modal" data-target="#removeProductModal" id="removeProductModalBtn" onclick="removeProduct('.$productId.')"> <i class="glyphicon glyphicon-trash"></i> Remove</a></li>       
+	    <li><a type="button" data-toggle="modal" data-target="#editPromotionModel" onclick="editBrands(' . $promoId . ')"> <i class="glyphicon glyphicon-edit"></i> Edit</a></li>
+	    <li><a type="button" data-toggle="modal" data-target="#removeMemberModal" onclick="removeBrands(' . $promoId . ')"> <i class="glyphicon glyphicon-trash"></i> Remove</a></li>       
 	  </ul>
 	</div>';
 
-	// $brandId = $row[3];
-	// $brandSql = "SELECT * FROM brands WHERE brand_id = $brandId";
-	// $brandData = $connect->query($sql);
-	// $brand = "";
-	// while($row = $brandData->fetch_assoc()) {
-	// 	$brand = $row['brand_name'];
-	// }
+		$output['data'][] = array(
+			$x,
+			$row[1],
+			$promoItemoutput,
+			$statusPromo,
+			$button
+		);
+		$x++;
+	} // /while 
 
-	$brand = $row[9];
-	$category = $row[10];
-
-	$imageUrl = substr($row[2], 3);
-	$productImage = "<img class='img-round' src='".$imageUrl."' style='height:30px; width:50px;'  />";
-
- 	$output['data'][] = array( 	
-		$productId,
- 		// image
- 		$productImage,
- 		// product name
- 		$row[1], 
- 		// rate
- 		$row[6],
- 		// quantity 
- 		$row[5], 		 	
- 		// brand
- 		$brand,
- 		// category 		
- 		$category,
- 		// active
- 		$active,
- 		// button
- 		$button 		
- 		); 	
- } // /while 
-
-}// if num_rows
+} // if num_rows
 
 $connect->close();
-
 echo json_encode($output);
