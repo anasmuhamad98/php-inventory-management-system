79a6d6c6d58adafe51b4b92b2ef42bad45a7d40e
AHMAD ANAS EZZAT BIN MUHAMAD <anasmuhamad98@gmail.com>
2021-01-21 21:45:05 +0800

    heroku

--- web/php_action/db_connect.php
+++ web/php_action/db_connect.php
@@ -13,7 +13,6 @@ $query_builder = TRUE;
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-
 $localhost = "localhost";
 $username = "root";
 $password = "";
