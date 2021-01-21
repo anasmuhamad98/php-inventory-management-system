f38ed98c1cc4a07cc5687d104a95eeb801687654
AHMAD ANAS EZZAT BIN MUHAMAD <anasmuhamad98@gmail.com>
2021-01-21 21:52:33 +0800

    heroku

--- php_action/db_connect.php
+++ php_action/db_connect.php
@@ -5,7 +5,7 @@ $cleardb_url      = parse_url(getenv("mysql://bb973c663f0808:5ac62113@us-cdbr-ea
 $cleardb_server   = $cleardb_url["us-cdbr-east-03.cleardb.com"];
 $cleardb_username = $cleardb_url["bb973c663f0808"];
 $cleardb_password = $cleardb_url["5ac62113"];
-$cleardb_db       = substr($cleardb_url["path"],1);
+$cleardb_db       = substr($cleardb_url["heroku_bbe94552e006391"],1);
 
 $active_group = 'default';
 $query_builder = TRUE;
--- web/php_action/db_connect.php
+++ web/php_action/db_connect.php
@@ -1,11 +1,11 @@
 <?php 	
 
 //Get Heroku ClearDB connection information
-$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server   = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db       = substr($cleardb_url["path"],1);
+$cleardb_url      = parse_url(getenv("mysql://bb973c663f0808:5ac62113@us-cdbr-east-03.cleardb.com/heroku_bbe94552e006391?reconnect=true"));
+$cleardb_server   = $cleardb_url["us-cdbr-east-03.cleardb.com"];
+$cleardb_username = $cleardb_url["bb973c663f0808"];
+$cleardb_password = $cleardb_url["5ac62113"];
+$cleardb_db       = substr($cleardb_url["heroku_bbe94552e006391"],1);
 
 $active_group = 'default';
 $query_builder = TRUE;
